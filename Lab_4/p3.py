869020940c0c3c16dd69700618c1b69d91850f3e
Brijesh Vadgama <23030501065@darshan.ac.in>
2024-07-04 19:59:27 +0530

    commit after completing lab 4

--- Lab_4/p3.py
+++ Lab_4/p3.py
@@ -1,1 +1,9 @@
 # 3. WAP to remove ith character from given string.
+
+s = input("Enter a string: ")
+i = int(input("Enter the position (index start from 0): "))
+
+new_str = s[:i] + s[i + 1 :]
+
+# Print the result
+print(f"The string after removing the character {i} is:, {new_str}")
